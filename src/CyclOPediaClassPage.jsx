45fab87b5acd9a21d1411a190cb28d0daab2a140
Umang Shrestha <umangstha124@gmail.com>
2023-11-27 23:21:12 +0545

    Display Instructor

--- src/CyclOPediaClassPage.jsx
+++ src/CyclOPediaClassPage.jsx
@@ -1,7 +1,51 @@
 import React from "react";
+import { getRandomUser } from "./Utility/api";
 
 class CyclPPediaClassPage extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      instructor: undefined,
+      studentList: [],
+      studentCount: 0,
+      hideInstructor: false,
+    };
+  }
+  componentDidMount = async () => {
+    const response = await getRandomUser();
+    console.log(response);
+    this.setState((prevState) => {
+      return {
+        instructor: {
+          name: response.data.first_name + " " + response.data.last_name,
+          email: response.data.email,
+          phone: response.data.phone_number,
+        },
+      };
+    });
+  };
+  componentDidUpdate() {
+    console.log("Component Did Update");
+  }
+  componentWillUnmount() {
+    console.log("Component Did Unmount");
+  }
+  render() {
+    console.log("Render Component");
+    return (
+      <div>
+        {this.state.instructor && (
+          <div className="p-3">
+            <span className="h4 text-success">Instructor</span>
+            <i className="bi bi-toggle-off btn btn-success btn-sm"></i>
+            <br />
+            Name: {this.state.instructor.name} <br />
+            Email: {this.state.instructor.email} <br />
+            Phone: {this.state.instructor.phone} <br />
+          </div>
+        )}
+      </div>
+    );
   }
 }
+export default CyclPPediaClassPage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Header from './Header';
+import CyclPPediaClassPage from './CyclOPediaClassPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Header />
+  <div>
+    <Header />
+    <div className='row text-white'>
+      <div className='col-6'>
+        <span className='h1 text-warning text-center'>Class Component</span>
+        <CyclPPediaClassPage />
+      </div>
+    </div>
+  </div>
 );
 
