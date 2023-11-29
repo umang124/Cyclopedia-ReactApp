9f3799991feb91d371ff49383c83370617e251df
Umang Shrestha <umangstha124@gmail.com>
2023-11-29 13:28:16 +0545

    Saving in LocalStorage

--- src/CyclOPediaClassPage.jsx
+++ src/CyclOPediaClassPage.jsx
@@ -4,32 +4,40 @@ import { getRandomUser } from "./Utility/api";
 class CyclOPediaClassPage extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
+    this.state = JSON.parse(localStorage.getItem("cyclopediaState")) || {
       instructor: undefined,
       studentList: [],
       studentCount: 0,
       hideInstructor: false,
-      inputName: "Name...",
+      inputName: "",
       inputFeedback: "",
     };
   }
   componentDidMount = async () => {
     console.log("Component Did Mount");
-    const response = await getRandomUser();
-    console.log(response);
-    this.setState((prevState) => {
-      return {
-        instructor: {
-          name: response.data.first_name + " " + response.data.last_name,
-          email: response.data.email,
-          phone: response.data.phone_number,
-        },
-      };
-    });
+
+    if (JSON.parse(localStorage.getItem("cyclopediaState"))) {
+      this.setState(JSON.parse(localStorage.getItem("cyclopediaState")));
+    } else {
+      const response = await getRandomUser();
+      console.log(response);
+      this.setState((prevState) => {
+        return {
+          instructor: {
+            name: response.data.first_name + " " + response.data.last_name,
+            email: response.data.email,
+            phone: response.data.phone_number,
+          },
+        };
+      });
+    }
   };
+
   componentDidUpdate() {
     console.log("Component Did Update");
+    localStorage.setItem("cyclopediaState", JSON.stringify(this.state));
   }
+
   componentWillUnmount() {
     console.log("Component Did Unmount");
   }
@@ -64,6 +72,7 @@ class CyclOPediaClassPage extends React.Component {
             Phone: {this.state.instructor.phone} <br />
           </div>
         )}
+
         <div className="p-3">
           <span className="h4 text-success">Feedback</span>
           <br />
@@ -71,7 +80,7 @@ class CyclOPediaClassPage extends React.Component {
             type="text"
             value={this.state.inputName}
             onChange={(e) => this.setState({ inputName: e.target.value })}
-            placeholder="Name..."
+            placeholder="Name"
           />
           <br />
           <textarea
@@ -80,6 +89,7 @@ class CyclOPediaClassPage extends React.Component {
             placeholder="Feedback"
           />
         </div>
+
         <div className="p-3">
           <span className="h4 text-success">Students</span>
           <br />
